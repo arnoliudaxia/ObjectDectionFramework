a7273a142327e5c363e389a1ee9cf7504cb84a83
Liuyifei <1456447215@qq.com>
2022-01-20 01:56:19 +0800

    设置的存储使用json
    简单多了

--- toolbox/setting.py
+++ toolbox/setting.py
@@ -1,32 +1,21 @@
 import numpy as np
+import json
 
 
-iniURL=r"C:\Users\Arnoliu\PycharmProjects\opecvtry\task\Color\color.ini"
+iniURL=r"C:\Users\Arnoliu\PycharmProjects\opecvtry\toolbox\color.ini"
 def readColorIni():
-    lower_red = None
-    upper_red = None
     f = open(iniURL, "r")
-    inis=f.readlines()
-    for ini in inis:
-        ini = ini.split(" ")
-        if(ini[0]=="color_bound"):
-            icol = [int(i) for i in ini[1:]]
-            lower_red = np.array(icol[0:3])
-            upper_red = np.array(icol[3:])
+    inis=json.loads(f.readline())
+    lower_red=np.array(inis["ColorBound"]["low"])
+    upper_red=np.array(inis["ColorBound"]["high"])
+
     return lower_red,upper_red
 def saveColorini(ColorBound):
-    ColorBound[0]=[str(i) for i in ColorBound[0]]
-    ColorBound[1]=[str(i) for i in ColorBound[1]]
     f = open(iniURL, "r+")
-    inis = f.readlines()
-    Towrite=[]
-    for ini in inis:
-        ini = ini.split(" ")
-        if (ini[0] == "color_bound"):
-            ini[1:4]=ColorBound[0]
-            ini[4:]=ColorBound[1]
-        ini=" ".join(ini)
-        Towrite.append(ini)
+    inis=json.loads(f.readline())
+    inis["ColorBound"]["low"]=ColorBound[0]
+    inis["ColorBound"]["high"]=ColorBound[1]
+
     f.seek(0)
-    f.writelines(Towrite)
+    f.writelines(json.dumps(inis))
     f.close()
